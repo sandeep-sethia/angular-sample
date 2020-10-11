7eac2b6316a8610cc2735b22ab4ad5f7d251289b
Sandeep Kumar <sandeep.kumar.sethia@accenture.com>
2020-10-11 21:47:55 +0800

    added service for the recipe and shopping list components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,46 @@
-import { EventEmitter } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply another test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Pasta',
+      'pink sause pasta',
+      'https://upload.wikimedia.org/wikipedia/commons/8/81/Pasta_e_fagioli_rapida.jpg',
+      [
+        new Ingredient('pasta', 1),
+        new Ingredient('tomato', 5),
+        new Ingredient('capsicum', 2),
+        new Ingredient('cheese', 1)
+      ]
+
+    ),
+    new Recipe(
+      'daal baati',
+      'awesome rajasthani dish',
+      'https://upload.wikimedia.org/wikipedia/commons/d/d8/Rajasthani_Dish.jpg',
+      [
+        new Ingredient('lentils', 1),
+        new Ingredient('flour', 2),
+        new Ingredient('onions', 2)
+      ]
+    )
   ];
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]): void {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,26 @@
+import { EventEmitter } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+
 export class ShoppingListService {
 
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
